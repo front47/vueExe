aa3eb0ba3c1080614b69bfd8d94df7afc372941c
ligh <1152613566@qq.com>
2017-09-10 17:18:37 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-Hello></v-Hello>
   </div>
 </template>
 
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Hello from './Hello'
+// import test from './test'
+
+export default {
+  Hello
+  // test
+}
+
