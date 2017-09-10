741dd864c58a7912857ff35bb16e51a72936cf94
Tony <1434103326@qq.com>
2017-09-10 17:04:04 +0800

    lalala

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-hello></v-hello>
+    <v-test></v-test>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Fastclick from 'fastclick'
+
+// 引入自定义组件
+import components from './components'
 
 Vue.config.productionTip = false
 
+window.onload = () => Fastclick.attach(document.body)
+
+Object.keys(components).forEach(k => {
+  let name = k.replace(/\w/, v => v.toUpperCase())
+  Vue.component(`v${name}`,components[k])
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
